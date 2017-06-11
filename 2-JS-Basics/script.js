cad98344969f5e3c6a6ef735f55abd456d5f52c6
kavish12 <kavishghime90@gmail.com>
2017-06-11 12:33:10 -0700

    end of basics

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -132,7 +132,7 @@ switch(job){
 */
 ////////////////////////////////////
 //Lecture: Functions
-
+/*
 function calculateAge(yearOfBirth){
     var age = 2016 - yearOfBirth;
     return age;
@@ -141,10 +141,192 @@ function calculateAge(yearOfBirth){
 var ageJohn = calculateAge(1990);
 var ageMike = calculateAge(1969);
 var ageMary = calculateAge(1948);
-console.log(ageMike);
+//console.log(ageMike);
+
+function yearsUnitlRetirement(name, yearsOfBirth){
+    var age = calculateAge(yearsOfBirth);
+    var retirement = 65 - age;
+
+    if(retirement >= 0){
+        console.log(name + " retires in " + retirement + " years");
+    }else{
+        console.log("Already Retired");
+    }
+
+}
+
+yearsUnitlRetirement('John', 1990);
+yearsUnitlRetirement('Mike', 1969);
+yearsUnitlRetirement('Mary', 1948);
+
+*/
+
+////////////////////////////////
+//Lecture: Statement and Expressions
+/*
+function someFun(par){
+    //code
+}
+
+var someFun =function(par){
+    //code
+}
+
+//Expressions
+3+4
+var x = 3;
+
+//statement
+if(x === 5){
+    //do soemthing
+}
+*/
+
+///////////////////////////////////////////
+//Lecture: Arrays
+/*
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[1]);
+
+names[1] = 'Ben';
+
+console.log(names);
 
 
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift("Mr. ");
+john.pop();
+john.shift();
+//alert(john.indexOf('Smith'));
+console.log(john);
+
+if(john.indexOf('Teacher') === -1){
+    console.log('John is NOT a teacher.');
+}
+*/
+
+/////////////////////////////////////
+//Lecture: Objects
+ //key values pairs in objects
+/*
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+};
 
 
+console.log(john.lastName);
+console.log(john['lastName']);
 
+var xyz = 'job';
+console.log(john[xyz]);
 
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+//different way to create object
+
+var jane = new Object();
+jane.name = 'jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+*/
+
+///////////////////////////////
+//Lecture: Objects and Methods
+/* v1.0
+ var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(){
+        return 2016 - this.yearOfBirth;
+    }
+};
+
+//this --> this means the object john
+//console.log(john.calculateAge(1990));
+
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
+console.log(john);
+*/
+//v2.0
+/*
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(){
+       this.age = 2016 - this.yearOfBirth;
+    }
+};
+
+john.calculateAge();
+console.log(john);
+
+*/
+
+/////////////////////////////////////////////////////
+//Lecture: Loops
+/*
+for(var i=0; i<10; i++){
+    //console.log(i);
+}
+
+/*
+0, true, print 0, update i to 1
+1, true, print 1, update i to 2
+.
+.
+.
+.
+9, true, print 9, update i to 10
+10, FALSE, end loop!
+*/
+/*
+var names = ['john', 'jane', 'mary', 'mark', 'bob'];
+/*
+for (var i=0; i<names.length; i++){
+    console.log(names[i]);
+}*/
+
+//while loops
+//condition evaluated before each condition
+/*
+var i=0;
+while(i < names.length){
+    console.log(names[i])
+    i++;
+}
+
+var i=0;
+while(i < names.length){
+    console.log(names[i])
+    i++;
+}
+//end of basics
+
+*/
