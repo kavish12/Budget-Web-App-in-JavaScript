75c671876fd7325d61b94771a5df58547dd2af44
kavish12 <kavishghime90@gmail.com>
2017-06-15 13:21:54 -0700

    Closures in JavaScript

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -190,3 +190,58 @@ game();
     console.log(score >= 5 - goodLuck);
 })(5);
 */
+
+///////////////////////////////////////////////////////////
+//Closures
+/*
+function retirement(retirementAge) {
+    var a = ' years left until retirment';
+    return function (yearOFBirth) {
+        var age = 2016 - yearOFBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+*/
+//retirement(66)(1990);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('what subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + " , what do you do?")
+        }
+    }
+}
+*/
+/*
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('what subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + " , what do you do?");
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
+*/
