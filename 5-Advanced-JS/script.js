1ea917d19fe886e087ebba7e77720b0d76415f37
kavish12 <kavishghime90@gmail.com>
2017-06-15 11:09:21 -0700

    Objects: Function prototype and Object.create

---
+++ 5-Advanced-JS/script.js
@@ -0,0 +1,71 @@
+//function constructor
+/*
+var john = {
+    name: 'john',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function () {
+    console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher'); //new object, instantiation
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+*/
+
+
+/////////////////////////////////////
+//object.create
+/*
+var personProto = {
+    calculateAge: function () {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: {
+        value: 'Jane'
+    },
+    yearOfBirth: {
+        value: 1969
+    },
+    job: {
+        value: 'designer'
+    }
+});
+*/
+
+////////////////////////////////////////////////////////////
+
+//primitives vs objects
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
