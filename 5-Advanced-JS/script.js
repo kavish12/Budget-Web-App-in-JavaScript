324422425440b3aebb31c5269f912c1f32b21b7f
kavish12 <kavishghime90@gmail.com>
2017-06-15 12:17:05 -0700

    Immediately Invoked Function Expression IIFE

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -64,8 +64,129 @@ var jane = Object.create(personProto, {
 ////////////////////////////////////////////////////////////
 
 //primitives vs objects
+/*
+//primitives
 var a = 23;
 var b = a;
 a = 46;
 console.log(a);
 console.log(b);
+
+//objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+
+//functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+////////////////////////////////////////////////////////
+//Passing functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartrate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartrate);
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+*/
+
+/////////////////////////////////////////////////////////
+//Functions returning functions
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('what subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + " , what do you do?")
+        }
+    }
+}
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('john');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
+*/
+
+//////////////////////////////////////////////////////
+//IIFE immideately invokd function expression
+/*
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+*/
+/*
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+//console.log(score);
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+*/
